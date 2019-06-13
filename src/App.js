16b3cc30e4b07ab0fe3d0f7ee75a3156651677ce
Rob Randell <robrandell@heckford-advertising.co.uk>
2019-06-13 17:36:36 +0100

    feat: flights added and tracked.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,179 @@
 import React, { Component } from 'react';
 import './App.css';
-import DeckGL, { IconLayer } from 'deck.gl';
+import DeckGL, { IconLayer, TextLayer } from 'deck.gl';
 import { StaticMap } from 'react-map-gl';
 import * as d3 from 'd3';
+import Base64 from 'base-64';
+import { CONFIG } from './config.js';
 
-const MAPBOX_ACCESS_TOKEN = '';
+import Airplane from './airplane-icon.png';
+import destinationPoint from './destinationPoint.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MAPBOX_ACCESS_TOKEN = CONFIG.mapboxapikey;
+const MAPBOX_STYLE = "mapbox://styles/robrandell/cj9jtcr56024l2rrxpdxuxmnf";
+const OPENSKY_USERNAME = CONFIG.openskyusername;
+const OPENSKY_PASSWORD = CONFIG.openskypassword;
+
+const initialViewState = {
+    longitude: -2.2936,
+    latitude: 54.496953,
+    zoom: 5,
+    pitch: 0,
+    bearing: 0
+}
+
+class App extends Component {
+    state = {
+        airplanes: []
+    };
+    currentFrame = null;
+    timer = null;
+    fetchEverySeconds = 10;
+    framesPerFetch = this.fetchEverySeconds * 30;
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    animationFrame = () => {
+        let { airplanes } = this.state;
+        airplanes = airplanes.map( d => {
+            const [ longitude, latitude ] = d.interpolatePos(
+                this.currentFrame / this.framesPerFetch
+            );
+            return {
+                ...d, longitude, latitude
+            };
+        });
+        this.currentFrame += 1;
+        this.setState({ airplanes });
+    }
+
+    startAnimation = () => {
+        if( this.timer ) {
+            this.timer.stop();
+        }
+        this.currentFrame = 0;
+        this.timer = d3.timer( this.animationFrame );
+    };
+
+    convertToMPH = ( metrespersecond ) => {
+        return Math.round( metrespersecond * 2.237 );
+    }
+
+    fetchData = () => {
+        d3.json( 'https://opensky-network.org/api/states/all?lamin=49.9289&lomin=-7.5062&lamax=58.589&lomax=1.6226', {
+            headers: new Headers({
+                'Authorization': `Basic ${Base64.encode(`${OPENSKY_USERNAME}:${OPENSKY_PASSWORD}`)}`
+            }),
+        }).then(
+            ({ states }) => this.setState({
+                airplanes: states.map( d => ({
+                    icao: d[ 0 ],
+                    callsign: d[ 1 ],
+                    longitude: d[ 5 ],
+                    latitude: d[ 6 ],
+                    velocity: this.convertToMPH( d[ 9 ] ),
+                    origin_country: d[ 2 ],
+                    true_track: Math.round( d[ 10 ] ),
+                    altitude: Math.round( d[ 7 ] ),
+                    interpolatePos: d3.geoInterpolate(
+                        [ d[ 5 ], d[ 6 ] ],
+                        destinationPoint( d[ 5 ], d[ 6 ], d[ 9 ] * this.fetchEverySeconds, d[ 10 ] )
+                    )
+                }))
+            }, () => {
+                this.startAnimation();
+                setTimeout(
+                    this.fetchData, this.fetchEverySeconds * 1000
+                );
+            })
+        );
+    };
+
+    render() {
+        const layers = [
+            new IconLayer({
+                id: 'airplanes',
+                data: this.state.airplanes,
+                pickable: true,
+                onClick: ( info, event ) => console.log( 'Clicked:', info.object.icao ),
+                iconAtlas: Airplane,
+                iconMapping: {
+                    airplane: {
+                        x: 0,
+                        y: 0,
+                        width: 512,
+                        height: 512
+                    }
+                },
+                sizeScale: 20,
+                getPosition: d => [ d.longitude, d.latitude, d.altitude ],
+                getIcon: d => 'airplane',
+                getAngle: d => Math.round( d.true_track + 135 )
+            }),
+            new TextLayer({
+                id: 'callsigns',
+                data: this.state.airplanes,
+                getPosition: d => [ d.longitude, d.latitude ],
+                getText: d => `${ d.callsign }`,
+                getAngle: 0,
+                getColor: [ 0, 225, 0, 255 ],
+                getTextAnchor: 'start',
+                getSize: 18,
+                fontFamily: 'Monaco, monospace',
+                fontWeight: 700,
+                getPixelOffset: [ 25, 0 ]
+            }),
+            new TextLayer({
+                id: 'airspeed',
+                data: this.state.airplanes,
+                getPosition: d => [ d.longitude, d.latitude ],
+                getText: d => `Airspeed: ${ d.velocity }mph`,
+                getAngle: 0,
+                getColor: [ 0, 225, 0, 255 ],
+                getTextAnchor: 'start',
+                getSize: 12,
+                fontFamily: 'Monaco, monospace',
+                fontWeight: 400,
+                getPixelOffset: [ 25, 18 ]
+            }),
+            new TextLayer({
+                id: 'bearing',
+                data: this.state.airplanes,
+                getPosition: d => [ d.longitude, d.latitude ],
+                getText: d => `Bearing: ${ d.true_track }deg`,
+                getAngle: 0,
+                getColor: [ 0, 225, 0, 255 ],
+                getTextAnchor: 'start',
+                getSize: 12,
+                fontFamily: 'Monaco, monospace',
+                fontWeight: 400,
+                getPixelOffset: [ 25, 34 ]
+            }),
+            new TextLayer({
+                id: 'altitude',
+                data: this.state.airplanes,
+                getPosition: d => [ d.longitude, d.latitude ],
+                getText: d => `Altitude: ${ d.altitude }ft`,
+                getAngle: 0,
+                getColor: [ 0, 225, 0, 255 ],
+                getTextAnchor: 'start',
+                getSize: 12,
+                fontFamily: 'Monaco, monospace',
+                fontWeight: 400,
+                getPixelOffset: [ 25, 50 ]
+            })
+        ];
+
+        return(
+            <div onClick = { this.renderToolTip }>
+                <DeckGL initialViewState = { initialViewState } controller = { true } layers = { layers } onClick = { this._onClick }>
+                    <StaticMap mapboxApiAccessToken = { MAPBOX_ACCESS_TOKEN } mapStyle = { MAPBOX_STYLE } />
+                </DeckGL>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/destinationPoint.js
@@ -0,0 +1,29 @@
+
+export default function destinationPoint( lon, lat, distance, bearing ) {
+    var radius = 6371e3;
+
+    var toRadians = function( v ) {
+        return v * Math.PI / 180;
+    }
+    var toDegrees = function( v ) {
+        return v * 180 / Math.PI;
+    }
+
+    var δ = Number( distance ) / radius;
+    var θ = toRadians( Number( bearing ) );
+
+    var φ1 = toRadians( Number( lat ) );
+    var λ1 = toRadians( Number( lon ) );
+
+    var sinφ1 = Math.sin( φ1 ), cosφ1 = Math.cos( φ1 );
+    var sinδ = Math.sin( δ ), cosδ = Math.cos( δ );
+    var sinθ = Math.sin( θ ), cosθ = Math.cos( θ );
+
+    var sinφ2 = sinφ1 * cosδ + cosφ1 * sinδ * cosθ;
+    var φ2 = Math.asin( sinφ2 );
+    var y = sinθ * sinδ * cosφ1;
+    var x = cosδ - sinφ1 * sinφ2;
+    var λ2 = λ1 + Math.atan2( y, x );
+
+    return [ ( ( toDegrees( λ2 ) + 540 ) % 360 ) - 180, toDegrees( φ2 ) ];
+}
